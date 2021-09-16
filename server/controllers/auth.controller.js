c671d5971dd5c534025c143f2946199b0ac1fa0a
Timotius Leonardo Lianoto <lionato14@gmail.com>
2021-09-16 17:33:09 +0700

    Register still have and error name is required

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -80,3 +80,46 @@ exports.registerController = (req, res) => {
         })
     }
 };
+
+exports.activationController = (req, res) => {
+    const { token } = req.body;
+
+    if(token) {
+        // Verify the token is valid or not or expired
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+            if(err) {
+                return res.status(401).json({
+                    error: 'Expired Token. Signup again'
+                })
+            } else {
+                // if valid save to database 
+        // Get name email password from token
+        const {name, email, password} = jwt.decode(token)
+
+        const user = new User({
+            name,
+            email,
+            password
+        })
+
+        user.save((err, user) => {
+            if(err) {
+                return res.status(401).json({
+                    error: errorHandler(err)
+                })
+            } else {
+                return res.json({
+                    success: true,
+                    message: 'Signup success',
+                    user
+                })
+            }
+        })
+            }
+        });
+    } else {
+        return res.json({
+            message: 'error happening please try again'
+        })
+    }
+}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// Validation
+const {
+    validRegister,
+    validLogin,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require('../helpers/valid');
+
 // Load Controller
 const {
-    registerController
+    registerController,
+    activationController
 } = require('../controllers/auth.controller.js');
 
-router.post('/register', registerController);
+router.post('/register', validRegister, registerController);
+router.post('/activation', activationController);
 
 module.exports = router;
