eb2c73d7a7ccabf24985882d24d4210ae2175736
TimotiusLeonardo <lionato14@gmail.com>
2021-10-18 10:16:42 +0700

    DLS-27/Feat: Able To Save page one

--- client/src/Screens/FormPage.jsx
+++ client/src/Screens/FormPage.jsx
@@ -1,21 +1,55 @@
-import React, { useEffect } from "react";
+import React from "react";
 import FirstForm from "../components/project_forms/FirstForm";
 import Navbar from "../components/Navbar";
-import { Container } from '@mui/material';
-import { useParams } from 'react-router';
+import { Container } from "@mui/material";
+import { useParams } from "react-router";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function FormPage() {
-  const onFormSubmit = (data) => {
-    console.log(data);
-  }
-
   const { projectid } = useParams();
+
+  const onFormSubmit = (data) => {
+    if (data.firstForm) {
+      console.log(data);
+      axios
+        .put(`${process.env.REACT_APP_API_URL}/updatepageone`, {
+          projectId: projectid,
+          project_name: data.firstForm.projectName,
+          project_desc: data.firstForm.projectDescription,
+          a_working_days: data.firstForm.workingDays,
+          a_typology: data.firstForm.buildingTypology.type,
+          a_location_province: "",
+          a_location_city: data.firstForm.location,
+          a_location_image: "",
+          a_gfa: data.firstForm.gfa,
+          a_floor_count: data.firstForm.floorNumber,
+          a_floor_height_avg: data.firstForm.avgFloorHeight,
+          a_occupancy_hours: data.firstForm.occupancyDensity,
+          a_operational_hours: data.firstForm.operationalHours,
+          a_holidays: data.firstForm.holidays,
+          a_ventilation_area: data.firstForm.areaOfVentilation,
+          a_orientation_image: [""],
+          a_micro_noise_image: "",
+          a_energy_place_image: "",
+          a_ach: 0,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            toast.success("Success Save Page One Draft");
+          }
+        });
+    }
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar isDashboard="true" />
       <Container maxWidth="xl">
-        <FirstForm projectId={projectid} onceSubmitted={(data) => onFormSubmit(data)} />
+        <FirstForm
+          projectId={projectid}
+          onceSubmitted={(data) => onFormSubmit(data)}
+        />
       </Container>
     </div>
   );
