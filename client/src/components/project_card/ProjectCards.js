1e527e6f988b37d6722c08e5e3ad62514014e1ba
TimotiusLeonardo <lionato14@gmail.com>
2021-10-19 21:00:20 +0700

    DLS-41/Feat: Circular progress gone when project is empty also

--- client/src/components/project_card/ProjectCards.js
+++ client/src/components/project_card/ProjectCards.js
@@ -1,46 +1,62 @@
-import React, {useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
 import { getUserId } from "../../helpers/auth";
-import { Grid, CircularProgress } from '@mui/material';
+import { Grid, CircularProgress } from "@mui/material";
 import ProjectCard from "./ProjectCard";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-function ProjectCards() {
+function ProjectCards({ isProjectEmpty, setIsProjectEmpty }) {
+  const [projects, setProjects] = useState([]);
 
-    const [projects, setProjects] = useState(
-        []
-      );
-    
-      useEffect(() => {
-        const userId = getUserId()
-        axios
-          .get(`${process.env.REACT_APP_API_URL}/getprojects`, {
-            params: {
-              id: userId
-            }
-          })
-          .then((result) => {
-            setProjects(result.data.projects)
-          })
-          .catch((err) => {
-            console.log(err);
-            toast.error('Something went wrong, please try again');
-          });
-      }, []);
+  const handleEmptyState = useCallback(
+    (event) => {
+      setIsProjectEmpty(event);
+    },
+    [setIsProjectEmpty]
+  );
 
-    return (
-        <div className="flex items-center justify-center">
-            {!projects.length ? <CircularProgress/> : 
-              <Grid container alignItems="stretch" spacing={4} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {projects.map((project) => (
-                    <Grid item xs={2} sm={4} md={4} key={project._id}>
-                        <ProjectCard project={project} projectId={project._id}/>
-                    </Grid>
-                ))}
-              </Grid>
-            }
-        </div>
-    )
+  useEffect(() => {
+    const userId = getUserId();
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/getprojects`, {
+        params: {
+          id: userId,
+        },
+      })
+      .then((result) => {
+        if (result.data.projects.length <= 0 || result.data === null || result.data === undefined) {
+          handleEmptyState(true);
+        } else {
+          handleEmptyState(false);
+          setProjects(result.data.projects);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Something went wrong, please try again");
+      });
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center">
+      {!projects.length ? (
+        isProjectEmpty ? null : <CircularProgress />
+      ) : (
+        <Grid
+          container
+          alignItems="stretch"
+          spacing={4}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {projects.map((project) => (
+            <Grid item xs={2} sm={4} md={4} key={project._id}>
+              <ProjectCard project={project} projectId={project._id} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
 }
 
-export default ProjectCards
+export default ProjectCards;
