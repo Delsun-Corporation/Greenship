0c6b6195b2fca598edd55267341cfe25e372b340
TimotiusLeonardo <lionato14@gmail.com>
2021-10-20 17:11:56 +0700

    DLS-36/Feat: Create API for page two

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -50,7 +50,7 @@ exports.getPageOneDraft = (req, res) => {
   const projectId = req.query.id;
   const objectId = mongoose.Types.ObjectId(projectId);
 
-  Project.findOne({_id: objectId})
+  Project.findOne({ _id: objectId })
     .select(
       "project_name project_desc project_status project_image project_date a_typology a_location_province a_location_city a_location_image a_gfa a_floor_count a_floor_height_avg a_occupancy_density a_operational_hours a_working_days a_holidays a_ventilation_area a_orientation_image a_micro_noise_image a_energy_place_image a_ach"
     )
@@ -62,7 +62,7 @@ exports.getPageOneDraft = (req, res) => {
         });
       } else {
         res.status(400).json({
-          message: "There is no such page"
+          message: "There is no such page",
         });
       }
     })
@@ -76,27 +76,29 @@ exports.getPageOneDraft = (req, res) => {
 exports.deleteProject = (req, res) => {
   const projectId = req.body.projectId;
   const objectId = ObjectId.ObjectId(projectId);
-  
-  Project.findByIdAndDelete(objectId).then((result) => {
-    if (!objectId) {
-      return res.status(400).json({
-        message: "Bad projectId"
-      })
-    } else if (!result) {
-      return res.status(401).json({
-        message: "No Project was found"
-      })
-    } else {
-      return res.status(200).json({
-        message: "Success Deleting Product"
-      })
-    }
-  }).catch((err) => {
-    res.status(402).json({
-      message: "Failed to delete product"
+
+  Project.findByIdAndDelete(objectId)
+    .then((result) => {
+      if (!objectId) {
+        return res.status(400).json({
+          message: "Bad projectId",
+        });
+      } else if (!result) {
+        return res.status(401).json({
+          message: "No Project was found",
+        });
+      } else {
+        return res.status(200).json({
+          message: "Success Deleting Product",
+        });
+      }
     })
-  })
-}
+    .catch((err) => {
+      res.status(402).json({
+        message: "Failed to delete product",
+      });
+    });
+};
 
 exports.updatePageOneDraft = (req, res) => {
   const projectId = req.body.projectId;
@@ -120,8 +122,8 @@ exports.updatePageOneDraft = (req, res) => {
     .then((project) => {
       if (!project) {
         res.status(400).json({
-          message: "Request Parameter is wrong"
-        })
+          message: "Request Parameter is wrong",
+        });
       }
       project.project_name = updatedTitle;
       project.project_desc = updatedDesc;
@@ -146,8 +148,67 @@ exports.updatePageOneDraft = (req, res) => {
       });
     })
     .catch((err) => {
-      res.status(400).json({
+      res.status(500).json({
         message: "Internal Server Error",
       });
     });
 };
+
+exports.updatePageTwoDraft = (req, res) => {
+  const { b_ottv, b_shgc, b_window_area, b_wall_area, projectId } = req.body;
+
+  const objectId = ObjectId.ObjectId(projectId);
+
+  if (req.body === {} || req.body === null || req.body === undefined) {
+    return res.status(400).json({
+      message: "Request parameter is wrong",
+    });
+  }
+
+  Project.findById(objectId)
+    .then((project) => {
+      if (!project) {
+        return res.status(400).json({
+          message: "Request Parameter is wrong",
+        });
+      }
+      project.b_ottv = b_ottv;
+      project.b_shgc = b_shgc;
+      project.b_window_area = b_window_area;
+      project.b_wall_area = b_wall_area;
+      project.project_date = new Date();
+      return project.save();
+    })
+    .then((result) => {
+      return res.status(200).json({
+        message: "Success save page two draft",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        message: "Internal server error",
+      });
+    });
+};
+
+exports.getPageTwoDraft = (req, res) => {
+  const { projectId } = req.query;
+  const objectId = ObjectId.ObjectId(projectId);
+
+  Project.findById(objectId).select('b_ottv b_shgc b_window_area b_wall_area').then(page_two => {
+    if (!page_two) {
+      return res.status(400).json({
+        message: "Bad Request"
+      })
+    }
+
+    return res.status(200).json({
+      page_two: page_two,
+      message: "Success getting page two draft"
+    })
+  }).catch(err => {
+    return res.status(500).json({
+      message: "Internal server error"
+    })
+  })
+}
--- server/routes/project.route.js
+++ server/routes/project.route.js
@@ -6,16 +6,20 @@ const {
     getProjects,
     getPageOneDraft,
     updatePageOneDraft,
-    deleteProject
+    deleteProject,
+    getPageTwoDraft,
+    updatePageTwoDraft
 } = require('../controllers/product.controller.js');
 
 router.post('/createproject', createProject);
 
 router.put('/updatepageone', updatePageOneDraft);
+router.put('/updatepagetwo', updatePageTwoDraft);
 
 router.delete('/deleteproject', deleteProject);
 
 router.get('/getprojects', getProjects);
 router.get('/getpageone', getPageOneDraft);
+router.get('/getpagetwo', getPageTwoDraft);
 
 module.exports = router;
