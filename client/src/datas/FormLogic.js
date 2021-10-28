22ad37f094b3a5ee9fa1f9b592f187e40797ba55
Christopher Kevin <mr.christopherkevin@gmail.com>
2021-10-28 21:11:57 +0700

    DLS-25/Fix: Temporarily remove math ceiling for better calculation

--- client/src/datas/FormLogic.js
+++ client/src/datas/FormLogic.js
@@ -10,19 +10,19 @@ export function calcNonOperatingHoursPerYear(operationalHours, workingDays, holi
 
 /// 1a6
 export function calcOccupancy(gfa, occupancyDensity) {
-    return Math.ceil((gfa * occupancyDensity))
+    return (gfa * occupancyDensity)
 }
 
 /// 1b1
 export function calcRoomVolumePerPerson(floorNumber, avgFloorHeight, occupancyDensity) {
-    return Math.ceil(floorNumber * avgFloorHeight / occupancyDensity)
+    return (floorNumber * avgFloorHeight / occupancyDensity)
 }
 
 /// 2
 export function calcWWR(collectionWindowArea, collectionWallArea) {
     const totalWindowArea = sumValue(collectionWindowArea);
     const totalWallArea = sumValue(collectionWallArea);
-    return Math.ceil((totalWindowArea / totalWallArea) * 100 / 100)
+    return (totalWindowArea / totalWallArea)
 }
 
 function sumValue(array) {
@@ -35,27 +35,27 @@ function sumValue(array) {
 
 /// 3a
 export function calcLightingEnergyConsumption(leOperationalDay, leOperationalNonDay, leNonOperational, gfa) {
-    return Math.ceil((leOperationalDay + leOperationalNonDay + leNonOperational)/gfa)
+    return (leOperationalDay + leOperationalNonDay + leNonOperational)/gfa
 }
 
 /// 3a2
 export function calcNonDaylightArea(gfa, daylightArea) {
-    return Math.ceil(gfa - daylightArea)
+    return (gfa - daylightArea)
 }
 
 /// 3a5
 export function calcLeDuringOperationalDay(daylightArea, lpdOperational, operationalHours) {
-    return Math.ceil(daylightArea * operationalHours * lpdOperational)
+    return (daylightArea * operationalHours * lpdOperational)
 }
 
 /// 3a6
 export function calcLeDuringOperationalNonDay(gfa, daylightArea, lpdOperational, operationalHours) {
-    return Math.ceil((gfa - daylightArea) * operationalHours * lpdOperational)
+    return (gfa - daylightArea) * operationalHours * lpdOperational
 }
 
 /// 3a7
 export function calcLeDuringNonOperational(gfa, operationalHours, workingDays, holidays, lpdNonOperational) {
-    return Math.ceil(gfa * calcNonOperatingHoursPerYear(operationalHours, workingDays, holidays) * lpdNonOperational)
+    return (gfa * calcNonOperatingHoursPerYear(operationalHours, workingDays, holidays) * lpdNonOperational)
 }
 
 /// 3b
@@ -68,94 +68,94 @@ export function calcBSL(windowAreas, wallAreas, windowHeatLoad, wallHeatLoad, to
         totalAreaHeatLoad += element * parseFloat(wallHeatLoad[index].value) * toTi
     });
 
-    return Math.ceil(totalAreaHeatLoad * toTi)
+    return (totalAreaHeatLoad * toTi)
 }
 
 export function calcPSL(gfa, occupancyDensity) {
-    return Math.ceil(200 * calcOccupancy(gfa, occupancyDensity))
+    return (200 * calcOccupancy(gfa, occupancyDensity))
 }
 
 export function calcPLL(gfa, occupancyDensity) {
-    return Math.ceil(250 * calcOccupancy(gfa, occupancyDensity))
+    return (250 * calcOccupancy(gfa, occupancyDensity))
 }
 
 export function calcLSL(gfa, totalLpdOperational, totalLpdNonOperational) {
-    return Math.ceil((totalLpdOperational + totalLpdNonOperational) * gfa * 1.25 * 3.4)
+    return ((totalLpdOperational + totalLpdNonOperational) * gfa * 1.25 * 3.4)
 } 
 
 export function calcCFM1(ach, gfa, floorCount, floorHeightAvg) {
-    return Math.ceil(ach * gfa * floorCount * floorHeightAvg * 35.31 / 60)
+    return (ach * gfa * floorCount * floorHeightAvg * 35.31 / 60)
 }
 
 export function calcCFM2(toTi, ach, gfa, floorCount, floorHeightAvg) {
     const cfm1 = calcCFM1(ach, gfa, floorCount, floorHeightAvg)
-    return Math.ceil(cfm1 * ((toTi * 1.08) + (0.15 * 0.67)))
+    return (cfm1 * ((toTi * 1.08) + (0.15 * 0.67)))
 }
 
 export function convertCoolingLoad(coolingLoad, operationalHours, workingDays, gfa) {
-    return Math.ceil(coolingLoad / 12000 * 1.12 * operationalHours * workingDays / gfa)
+    return (coolingLoad / 12000 * 1.12 * operationalHours * workingDays / gfa)
 }
 
 /// 3c
 export function calcApplianceConsumption(amount, watt, operationalHours) {
-    return Math.ceil(amount * watt * operationalHours)
+    return (amount * watt * operationalHours)
 }
 
 /// 3d
 export function calcLiftConsumption(gfa, operationalHours, watt, amount, capacity, velocity) {
-    return Math.ceil(0.2 * watt * amount * ((0.75 * capacity * velocity) / 75) * 0.746 * operationalHours / gfa)
+    return (0.2 * watt * amount * ((0.75 * capacity * velocity) / 75) * 0.746 * operationalHours / gfa)
 }
 
 export function calcUtilityConsumption(gfa, operationalHours, watt, amount) {
-    return Math.ceil(watt * amount * operationalHours / gfa)
+    return (watt * amount * operationalHours / gfa)
 }
 
 /// 3e
 export function calcPlugEnergyAC(gfa, operationalHours, operatingPower) {
-    return Math.ceil(operatingPower * operationalHours * gfa)
+    return (operatingPower * operationalHours * gfa)
 }
 
 export function calcPlugEnergyNonAC(gfa, operationalHours, nonOperatingPower) {
-    return Math.ceil(nonOperatingPower * (24 - operationalHours) * gfa)
+    return (nonOperatingPower * (24 - operationalHours) * gfa)
 }
 
 export function calcPlugConsumption(gfa, plugEnergyAC, plugEnergyNonAC) {
-    return Math.ceil((plugEnergyAC + plugEnergyNonAC) / gfa)
+    return ((plugEnergyAC + plugEnergyNonAC) / gfa)
 }
 
 /// 4a
 export function calcZonePopulation(gfa, occupancyDensity) {
-    return Math.ceil((gfa * occupancyDensity))
+    return ((gfa * occupancyDensity))
 }
 
 export function calcVbz(rp, pz, ra, az) {
-    return Math.ceil((rp * pz) + (ra * az))
+    return ((rp * pz) + (ra * az))
 }
 
 /// 4b
 export function calcACH(velocity, ventilationArea, volume) {
-    return Math.ceil((velocity * ventilationArea * 3600) / volume)
+    return ((velocity * ventilationArea * 3600) / volume)
 }
 
 /// 4c
 export function calcAccessPercentage(accessArea, gfa) {
-    return Math.ceil(accessArea / gfa * 100)
+    return (accessArea / gfa * 100)
 }
 
 /// 4d
 export function calcIlluminance(area, count, lumen) {
-    return Math.ceil((count * lumen * 0.56) / area)
+    return ((count * lumen * 0.56) / area)
 }
 
 /// 5
 export function calcPotentialPV(pca, l, w) {
-    return Math.ceil(pca / (l * w)) 
+    return (pca / (l * w)) 
  }
  
  export function calcPredictionElectical(potentialPv, wpeak) {
-     return Math.ceil(potentialPv * wpeak * 4 * 365)
+     return (potentialPv * wpeak * 4 * 365)
  }
  
  export function calcPercentageElectrical(predictialElectrical) {
-     return Math.ceil(predictialElectrical/3 * 100 / 100)
+     return (predictialElectrical/3 * 100 / 100)
  }
